bbb84aa140ed3b1bf46347b965e5b3576d25aef3
Ralyton <ton_trompa07@hotmail.com>
2023-02-24 22:25:25 -0300

    atualizei gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,39 @@
-function funcaoPadrao(callback) {
-    console.log('Executando via Gulp')
-    callback()
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const sourcemaps = require('gulp-sourcemaps')
+
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'))
 }
 
-function dizOi() {
-    console.log('Oi Gulp')
-    dizTchau()
+function funcaoPadrao(callback) {
+    setTimeout(function() {
+        console.log('Executando via Gulp')
+        callback()
+    }, 2000)
+}
+
+function dizOi(callback) {
+    setTimeout(function() {
+        console.log('Oi Gulp')
+        dizTchau()
+        callback()
+    }, 1000)
 }
 
 function dizTchau() {
     console.log('Tchau Gulp')
 }
 
-exports.default = funcaoPadrao
+exports.default = gulp.parallel(funcaoPadrao, dizOi)
 exports.dizOi = dizOi
+exports.sass = compilaSass
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass))
+}
